40c5f94fd0ca7908797afcc974fe19cf1438b23e
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-26 05:56:09 -0700

    comments model

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,9 +1,16 @@
 namespace api.Models
 {
+    // [Table("Comments")]
     public class Comment
     {
+         public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
        public int? StockId{ get; set; } 
        //navigation property
        public Stock? Stock{get;set;}
+        // public string AppUserId { get; set; }
+        // public AppUser AppUser { get; set; }
     }
 }
