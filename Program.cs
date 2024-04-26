1045f740f9ee63de2ee23c587a4bb9c6e7f00925
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-26 05:47:25 -0700

    stockes model

--- Program.cs
+++ Program.cs
@@ -20,3 +20,4 @@ app.UseHttpsRedirection();
 
 app.Run();
 
+
--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c57a5e4884f8488e8fd8074031434d93730726e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
