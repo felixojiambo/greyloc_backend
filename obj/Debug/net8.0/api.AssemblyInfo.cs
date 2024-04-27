cc2736e60511ad415004ae0228b6947ac64336f6
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-27 05:29:40 -0700

    comment repo and interface

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b0f3c5d433bcb0cd722203ba9ff344f8336e1a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa529379572f4a6c43062b458cc9660339ff263")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
