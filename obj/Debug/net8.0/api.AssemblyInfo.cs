79833f061a959189aca3f71d16d97f83c98022db
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-27 03:07:06 -0700

    delete stock

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b0a8a7fd78061da79f52644e1b16576ed73712")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b37c1af0a738fb75dc28af88698318293fc7d82d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
