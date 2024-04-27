b37c1af0a738fb75dc28af88698318293fc7d82d
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-27 02:58:45 -0700

    update controller and dto

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -5,40 +5,61 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    [Route("api/stock")]
-    [ApiController]
-    public class StockController(ApplicationDbContext context) : ControllerBase
-    {
-        private readonly ApplicationDbContext _context = context;
+  [Route("api/stock")]
+  [ApiController]
+  public class StockController(ApplicationDbContext context) : ControllerBase
+  {
+    private readonly ApplicationDbContext _context = context;
 
-        //using primary constructor for the bellow code instead
-//  public StockController(ApplicationDbContext context)
-//       {
-//         _context=context;
-//       } 
-        [HttpGet]
-      public IActionResult GetAll(){
-        //deffered execution
-        var stocks=_context.Stocks.ToList()
-        .Select(s=>s.ToStockDto());
-        return Ok(stocks);
-      } 
-      [HttpGet("{id}")]
-      public IActionResult GetById([FromRoute] int id)
-      {
-       var stock=_context.Stocks.Find(id);
-       if(stock==null){
-        return NotFound();
-       }
-       return Ok(stock.ToStockDto());
-      }
-      [HttpPost]
-      public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
-      {
-        var stockModel=stockDto.ToStockFromCreateDTO();
-        _context.Stocks.Add(stockModel);
-        _context.SaveChanges();
-        return CreatedAtAction(nameof(GetById),new {id=stockModel.Id},stockModel.ToStockDto());
-      }
+    //using primary constructor for the bellow code instead
+    //  public StockController(ApplicationDbContext context)
+    //       {
+    //         _context=context;
+    //       } 
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+      //deffered execution
+      var stocks = _context.Stocks.ToList()
+      .Select(s => s.ToStockDto());
+      return Ok(stocks);
     }
+    [HttpGet("{id}")]
+    public IActionResult GetById([FromRoute] int id)
+    {
+      var stock = _context.Stocks.Find(id);
+      if (stock == null)
+      {
+        return NotFound();
+      }
+      return Ok(stock.ToStockDto());
+    }
+    [HttpPost]
+    public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+    {
+      var stockModel = stockDto.ToStockFromCreateDTO();
+      _context.Stocks.Add(stockModel);
+      _context.SaveChanges();
+      return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+    {
+      var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+      if (stockModel == null)
+      {
+        return NotFound();
+      }
+      stockModel.Symbol = updateDto.Symbol;
+      stockModel.CompanyName = updateDto.CompanyName;
+      stockModel.Purchase = updateDto.Purchase;
+      stockModel.LastDiv = updateDto.LastDiv;
+      stockModel.Industry = updateDto.Industry;
+      stockModel.MarketCap = updateDto.MarketCap;
+      _context.SaveChanges();
+      return Ok(stockModel.ToStockDto());
+    }
+  }
 }
--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a9e5e2727546382362ddda44cafae4ec6cb875")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b0a8a7fd78061da79f52644e1b16576ed73712")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
