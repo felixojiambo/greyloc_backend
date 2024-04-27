ce48ab054b1a01e3c7d4136f1a874a09af8552f5
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-27 05:52:42 -0700

    get all comments

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa529379572f4a6c43062b458cc9660339ff263")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c8e9525e5c846bae603b0d80e57ef8b0f80d95d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
